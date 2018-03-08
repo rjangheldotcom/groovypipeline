9d74dc87ca0fd457dd0af680a3be37d8763e339e
root <root@ip-172-21-0-24.ec2.internal>
2018-03-08 14:17:00 +0000

    update

--- backend.groovy
+++ backend.groovy
@@ -60,6 +60,20 @@ node
        sh  'sudo docker run --rm  --memory="1400m" --cpus=0.250 --name altidemoqa -d -p 8082:8080 vkotteswaran/$JOB_BASE_NAME:$BUILD_ID'
     }
 
+ stage('Deploy to Prod?') {
+        timeout(time: 35000, unit: 'SECONDS'){
+        input message: 'Need approval to proceed...', ok: 'Approve'
+        input message: 'Are you Sure?', ok: 'yes'
+             mail body: 'Approved to Deploy in Prod',
+                     from: 'smohanram@altimetrik.com',
+                     replyTo: 'smohanram@altimetrik.com',
+                     subject: 'Approved to Deploy in PROD',
+                     to: 'smohanram@altimetrik.com'
+        }
+        sh 'sudo docker stop altidemoprod || true && sudo docker rm altidemoprod || true'
+        sh 'sudo docker run --rm --memory="1400m" --cpus=0.250 --name altidemoprod -d -p 8083:8080 vkotteswaran/$JOB_BASE_NAME:$BUILD_ID'
+    }
+
     }
 } catch (e){
         echo "${e}"
